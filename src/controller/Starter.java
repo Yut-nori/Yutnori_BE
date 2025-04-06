1c5415575fd69b748c58604293272c85556951f3
sweetbolly <swjjaang@naver.com>
2025-04-06 23:49:36 +0900

    feat: starter 메소드 작성 및 attribute 수정

--- src/controller/Starter.java
+++ src/controller/Starter.java
@@ -1,19 +1,30 @@
 package controller;
 
 public class Starter {
-    private int numPlayer;
-    private String[] playerNameList;
-    private int playerUnitNum;
-    private int boardEdgeNum;
+    private  int numPlayer;
+    private  String[] playerNameList;
+    private  int playerUnitNum;
+    private  int boardEdgeNum;
+    private  PlayManager playerManager;
+    private  BoardManager boardManager;
 
-    public Starter(int numPlayer, String[] playerNameList, int playerUnitNum) {
+    private static Starter instance;
+    public Starter(int numPlayer, String[] playerNameList, int playerUnitNum, int boardEdgeNum) {
         this.numPlayer = numPlayer;
         this.playerNameList = playerNameList;
         this.playerUnitNum = playerUnitNum;
+        this.boardEdgeNum = boardEdgeNum;
+        this.playerManager = new PlayManager(numPlayer);
+        this.boardManager = new BoardManager(boardEdgeNum);
+    }
+
+    public static void init(int numPlayer, String[] playerNameList, int playerUnitNum, int boardEdgeNum) {
+        instance = new Starter(numPlayer, playerNameList, playerUnitNum, boardEdgeNum);
     }
 
     public static void start() {
-
+        instance.boardManager.createBoard();
+        instance.playerManager.createPlayer(instance.playerNameList, instance.playerUnitNum);
+        instance.playerManager.controlTurn();
     }
-
 }
