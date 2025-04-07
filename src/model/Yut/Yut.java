c7236278e4be2151029aba89490bdb7f1e321d2d
SEUNGHWA LEE <sw092311@cau.ac.kr>
2025-04-07 11:49:08 +0900

    Yut Class 완성

--- src/model/Yut/Yut.java
+++ src/model/Yut/Yut.java
@@ -1,14 +1,29 @@
 package model.Yut;
 
+import java.util.Random;
+
 public class Yut {
     private boolean isHead;
     private boolean isTail;
 
-    public void setHead() {
-        // 확률 처리
+    public void setHead() { // 확률 처리
+        Random rand = new Random();
+        // 0이면 head, 1이면 tail
+        if (rand.nextBoolean()) {
+            isHead =true;
+            isTail = false;
+        }
+        else {
+            isHead = false;
+            isTail = true;
+        }
     }
 
     public boolean getHead() {
         return isHead;
+    }
+
+    public boolean getTail() {
+        return isTail;
     }
 }
