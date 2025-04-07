6dd0faf30006d286fe3a3b18c80c78a9d58237fe
SEUNGHWA LEE <sw092311@cau.ac.kr>
2025-04-07 12:09:40 +0900

    Yut Class 완료

--- src/model/Yut/YutResult.java
+++ src/model/Yut/YutResult.java
@@ -1,10 +1,16 @@
 package model.Yut;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class YutResult {
-    private static List<Yut> yutList;
+    private static List<Yut> yutList = new ArrayList<Yut>();
     private static int yutResult;
+
+    // 윷 4개 생성. 나중에 윷 개수를 여기서 지정하면 됨.
+    // 한 개의 윷은 빽도로 지정
+    int backDoIndex = new java.util.Random().nextInt(4);
+
 
     public static int getYutResult() {
         return yutResult;
