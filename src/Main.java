d09c2b6f084dbf0a2ab5215c5f258c72c2b37670
SEUNGHWA LEE <103622094+lshwa@users.noreply.github.com>
2025-04-09 11:50:48 +0900

    Board/Position 로직 구현 및 Main Test Code 추가 완료 (#4)
    Co-authored-by: Jeong Jae Lee <lee1026td@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,37 @@
+import model.Player;
 import model.board.Board;
+import model.board.Position;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Board b = new Board(4);
-        b.print();
-        b.backPrint();
+        Player p = new Player("test", 1, false);
+
+        Position[] positions = b.getPositionArr();
+
+        Position currentPos = positions[0];
+
+        List<Integer> result = new ArrayList<>();
+
+        /* test용 윷 결과 */
+        int[] yutRes = {4, 4, 2, 3};
+        for(int res : yutRes) {
+            result.add(res);
+        }
+
+        for(int a : result) {
+            System.out.print("윷 결과 : " + a + " ");
+
+
+            if(a > 0) {
+                currentPos = currentPos.testMove(a);
+            }
+
+            System.out.println("현재 위치 : " + currentPos.getIndex() + " | isVertex : " + currentPos.isVertex());
+        }
+
     }
 }
