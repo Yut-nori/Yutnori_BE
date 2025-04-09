4e6ae781ca5f60c463c17a0991e056df6b618eb2
sweetbolly <swjjaang@naver.com>
2025-04-10 04:12:29 +0900

    feat: Starter 완성

--- src/controller/Starter.java
+++ src/controller/Starter.java
@@ -1,30 +1,42 @@
 package controller;
 
+import model.Unit;
+
+import java.util.Scanner;
+
 public class Starter {
     private  int numPlayer;
     private  String[] playerNameList;
     private  int playerUnitNum;
     private  int boardEdgeNum;
-    private  PlayManager playerManager;
-    private  BoardManager boardManager;
 
-    private static Starter instance;
+    private PlayManager playManager;
+
     public Starter(int numPlayer, String[] playerNameList, int playerUnitNum, int boardEdgeNum) {
         this.numPlayer = numPlayer;
         this.playerNameList = playerNameList;
         this.playerUnitNum = playerUnitNum;
         this.boardEdgeNum = boardEdgeNum;
-        this.playerManager = new PlayManager(numPlayer);
-        this.boardManager = new BoardManager(boardEdgeNum);
     }
 
-    public static void init(int numPlayer, String[] playerNameList, int playerUnitNum, int boardEdgeNum) {
-        instance = new Starter(numPlayer, playerNameList, playerUnitNum, boardEdgeNum);
-    }
+    // 보드 매니저 초기화 및 보드 생성
+    // 플레이어 매니저 생성
+    // 플레이어 리스트 생성
+    // 구조 변경[2025.04.09]: static start -> start
+    // 게임 재실행 과정에서 파라미터 유연하게 받을 필요가 있다고 판단.
+    public void start() {
+        this.playManager = new PlayManager(numPlayer, boardEdgeNum, playerNameList, playerUnitNum);
+        while (!playManager.checkEnd()) {
+            playManager.controlTurn();
+        }
 
-    public static void start() {
-        instance.boardManager.createBoard();
-        instance.playerManager.createPlayer(instance.playerNameList, instance.playerUnitNum);
-        instance.playerManager.controlTurn();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("게임을 다시 시작하려면 'r', 종료하려면 'q'를 입력하세요.");
+        String input = scanner.nextLine();
+        if (input.equalsIgnoreCase("r")) {
+            End.retry();
+        } else {
+            End.quit();
+        }
     }
 }
