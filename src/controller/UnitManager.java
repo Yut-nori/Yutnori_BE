91e0fe03a18ac0bfc36f159aee1be597f90728dc
sweetbolly <swjjaang@naver.com>
2025-04-10 23:26:06 +0900

    feat: UnitManager 기능 수정 -> Unit 움직임 총괄

--- src/controller/UnitManager.java
+++ src/controller/UnitManager.java
@@ -6,6 +6,8 @@ import model.GroupUnit;
 import model.Player;
 import model.Unit;
 
+
+//-> Board 가 가져야할 역할까지 가지고 있음 -> 통합해서 분리 필요.ㄴ
 public class UnitManager {
     private List<GroupUnit> groupList; // 그룹 목록
 
@@ -14,13 +16,9 @@ public class UnitManager {
     }
 
     // 새로운 그룹 생성 및 추가
-    public GroupUnit createGroup(Player player, Unit unit, int currentPositionIdx) {
-        unit.setPositionIdx(currentPositionIdx);
-        unit.setStatus(Unit.Status.ON);
-
+    public GroupUnit createGroup(Player player, Unit unit) {
         List<Unit> units = new ArrayList<>();
         units.add(unit);
-
         GroupUnit group = new GroupUnit(player, units);
         groupList.add(group);
         return group;
@@ -28,11 +26,10 @@ public class UnitManager {
 
     // 그룹 이동
     public void moveGroup(GroupUnit group, int moveDistance) {
-        int newPos = group.getCurrentPositionIdx() + moveDistance;
-        group.setCurrentPositionIdx(newPos);
         for (Unit unit : group.getUnitGroup()) {
-            unit.setPositionIdx(newPos);
+            unit.setStatus(Unit.Status.ON);
         }
+        group.setPositionIdx(group.getPositionIdx() + moveDistance);
     }
 
     // 특정 플레이어의 모든 그룹 반환
@@ -46,24 +43,15 @@ public class UnitManager {
         return result;
     }
 
-    // 특정 플레이어의 모든 유닛이 종료 상태인지 확인
-    public boolean allUnitsEnded(Player player, int totalUnits) {
-        int endedUnits = 0;
-        for (GroupUnit group : getGroupsByPlayer(player)) {
-            for (Unit unit : group.getUnitGroup()) {
-                if (unit.getStatus() == Unit.Status.END) {
-                    endedUnits++;
-                }
-            }
-        }
-        return endedUnits == totalUnits;
-    }
-
     // 이동한 자리에 우리 유닛이 있는지 확인, 병합
-    public boolean isFriendlytInPosition(int position, Player current) {
+    public boolean isFriendlytInPosition(Player current, int position) {
         GroupUnit currentGroup = null;
+        int findEqual = 0;
         for (GroupUnit group : getGroupsByPlayer(current)) {
-            if (group.getCurrentPositionIdx() == position) {
+            if(group.getPositionIdx() == position){
+                findEqual++;
+            }
+            if(findEqual >= 2) {
                 currentGroup = group;
                 mergeGroups(currentGroup, position);
                 return true;
@@ -74,23 +62,27 @@ public class UnitManager {
 
     // 이동한 자리에 상대 유닛이 있는지 확인, 병합
     public boolean isEnemytInPosition(Player current, int position) {
+        System.out.println("현재 플레이어 : "+current.getPlayerName() + ", 현재 위치 : "+position);
         GroupUnit currentGroup = null;
         for (GroupUnit group : groupList) {
-            if (group.getCurrentPositionIdx() == position && !group.getPlayer().getPlayerName().equals(current.getPlayerName())) {
+            if (group.getPositionIdx() == position && !group.getPlayer().getPlayerName().equals(current.getPlayerName())) {
+                System.out.println("상대 플레이어 : " +group.getPlayer().getPlayerName());
                 for (Unit unit : group.getUnitGroup()) {
                     unit.setStatus(Unit.Status.READY);
+                    createGroup(group.getPlayer(), unit);
                 }
                 groupList.remove(group);
                 return true;
             }
         }
+        System.out.println("not enemy player");
         return false;
     }
 
     // 그룹 병합
     private void mergeGroups(GroupUnit targetGroup, int position) {
         for (GroupUnit group : groupList) {
-            if (group.getCurrentPositionIdx() == position && group != targetGroup) {
+            if (group.getPositionIdx() == position && group != targetGroup) {
                 targetGroup.getUnitGroup().addAll(group.getUnitGroup());
                 groupList.remove(group);
                 break;
