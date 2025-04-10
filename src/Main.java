4425a6daddcb8e9a71aaecb1e21917049857a64b
sweetbolly <swjjaang@naver.com>
2025-04-10 23:30:18 +0900

    feat: PlayManager 수정/Main에 테스트 코드 수정 - Control Turn 구현 -> 테스트용 - Board와 연동 X , 통합하면서 다시 확인해봐야함 - 게임 로직을 총괄 중

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,33 @@
-import model.board.Board;
 import controller.Starter;
-import java.util.List;
-
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Board b = new Board(4);
-        b.print();
-        b.backPrint();
+       /*  Board b = new Board(4);
+        Player p = new Player("test", 1, false);
 
+        Position[] positions = b.getPositionArr();
 
-        /*Starter_test_CLI version Test Code*/
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("플레이어 수를 입력하세요(최소 2명, 최대 4명");
-        int numPlayer = scanner.nextInt();
-        scanner.nextLine();
+        Position currentPos = positions[0];
 
-        String[] playerNameList = new String[numPlayer];
-        for(int i = 0; i < numPlayer; i++){
-            System.out.print("플레이어" + (i+1) +  "이름 입력" );
-            playerNameList[i] = scanner.nextLine();
+        List<Integer> result = new ArrayList<>();
+
+        //test용 윷 결과
+        int[] yutRes = {4, 4, 2, 3};
+        for(int res : yutRes) {
+            result.add(res);
         }
 
-        System.out.print("유닛의 개수를 입력하세요(최소 2개, 최대 5개");
-        int playerUnit = scanner.nextInt();
-        scanner.nextLine();
+        for(int a : result) {
+            System.out.print("윷 결과 : " + a + " ");
 
-        System.out.print("게임판 유형을 선택하세요(기본 4) : ");
-        int boardEdgeNum = scanner.nextInt();
 
-        System.out.print("");
+            if(a > 0) {
+                currentPos = currentPos.testMove(a);
+            }
 
-        Starter starter = new Starter(numPlayer,playerNameList,playerUnit,boardEdgeNum);
-        starter.start(); // 설정된 값으로 게임 시작
-
+            System.out.println("현재 위치 : " + currentPos.getIndex() + " | isVertex : " + currentPos.isVertex());
+        }*/
+        Starter starter = new Starter(2,  new String[] {"test1", "test2"}, 2,4);
+        starter.start(); // Example usage to avoid "never read" warning
     }
 }
